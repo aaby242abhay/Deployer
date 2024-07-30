96f62a5078311e15a28e523d756753a3aa3320b6
Aaby Abhay <aaby242abhay@gmail.com>
2024-07-30 22:02:29 +0530

    adding middlewares in HONO

--- hono-app/src/index.ts
+++ hono-app/src/index.ts
@@ -2,11 +2,19 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+async function middleware(c : any, next : any){
+  if(c.req.header("Authorization")){
+    await next();
+  }else{
+    return c.text('Unauthorized');
+  }
+}
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.post('/', async (c) => {
+app.post('/', middleware, async (c) => {
   const body = await c.req.json()
   console.log(body);
   console.log(c.req.header("Authorization"));
