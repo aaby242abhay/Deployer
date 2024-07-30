eb7784265b937c61556c5fb475da06868a835e13
Aaby Abhay <aaby242abhay@gmail.com>
2024-07-30 20:48:37 +0530

    adding hono for workers

---
+++ hono-app/src/index.ts
@@ -0,0 +1,19 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/', async (c) => {
+  const body = await c.req.json()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
+  return c.text('This is the response from the post req in HONO!!!')
+})
+
+export default app
+
